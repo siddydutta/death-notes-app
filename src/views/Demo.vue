6a4c5cfe9ab11b855dffb6dd26ce81555cf03bc6
Arshia Kaul <2976917K@student.gla.ac.uk>
2025-03-05 22:30:12 +0000

    Fix: Buggy Demo

---
+++ src/views/Demo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <AppBar />
+    </div>
+</template>
+
+<script>
+import AppBar from '@/components/AppBar.vue';
+
+export default {
+    name: 'Demo',
+    components: {
+        AppBar
+    }
+}
+</script>
+
+<style scoped>
+/* Add any styles if needed */
+</style>
